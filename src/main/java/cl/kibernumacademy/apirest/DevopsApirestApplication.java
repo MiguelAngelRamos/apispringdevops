01dec2208a98dcc81c1aa23ad6371e952fcba1bf
Miguel Ramos <return.infox@gmail.com>
2023-06-25 12:45:44 -0400

    actualizacion con h2

--- src/main/java/cl/kibernumacademy/apirest/DevopsApirestApplication.java
+++ src/main/java/cl/kibernumacademy/apirest/DevopsApirestApplication.java
@@ -1,13 +1,32 @@
 package cl.kibernumacademy.apirest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DevopsApirestApplication {
+import cl.kibernumacademy.apirest.models.dao.IStudentDao;
+import cl.kibernumacademy.apirest.models.entity.Student;
 
+@SpringBootApplication
+public class DevopsApirestApplication implements CommandLineRunner{
+
+  private final IStudentDao repository;
+  @Autowired
+  public DevopsApirestApplication(IStudentDao repository) {
+    this.repository = repository;
+  }
   public static void main(String[] args) {
     SpringApplication.run(DevopsApirestApplication.class, args);
   }
+
+@Override
+ public void run(String... args) throws Exception {
+  Student student1 = new Student("Miguel", "Ramos", "mramoscli@correo.com");
+  Student student2 = new Student("Camila", "Marquez", "cmarquez@correo.com");
+   repository.save(student1);
+   repository.save(student2);
+
+ }
 
 }
